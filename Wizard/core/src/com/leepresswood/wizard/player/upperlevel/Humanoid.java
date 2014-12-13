17756f46536666740ba6000010f99c8f5ae5536d
Lee Presswood <pressedlee123@gmail.com>
2014-12-13 01:31:39 -0600

    Added spritesheet logic. Doesn't work.

--- Wizard/core/src/com/leepresswood/wizard/player/upperlevel/Humanoid.java
+++ Wizard/core/src/com/leepresswood/wizard/player/upperlevel/Humanoid.java
@@ -5,39 +5,55 @@
  */
 package com.leepresswood.wizard.player.upperlevel;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.leepresswood.wizard.Assets;
-import com.leepresswood.wizard.player.bodyparts.Body;
-import com.leepresswood.wizard.player.bodyparts.Hand;
-import com.leepresswood.wizard.player.bodyparts.Head;
 import com.leepresswood.wizard.screen.ScreenGame;
 
 public abstract class Humanoid extends Player
 {
-	public Head head;
-	public Body body;
-	public Hand hand;
+	public Sprite sprite;
+	
+	private float animation_timer = 0f;
+	private final float animation_tick = 1.5f;		//Length of time for one complete stride before looping.
+	private int current_animation_counter = 0;
+	protected TextureRegion[] left_walk;
+	protected TextureRegion[] right_walk;
+	
+	private boolean is_facing_left = false;
 	
 	public Humanoid(ScreenGame screen)
 	{
 		super(screen);
+		
+		//Grab the spritesheet's frames
+		Texture sheet = screen.game.assets.getTexture(Assets.TEXTURE_PERSON_SPRITES);
+		TextureRegion[][] temporary = TextureRegion.split(sheet, 52, 81);
+		
+		//Split them into left and right walking frames.
+		left_walk = new TextureRegion[temporary.length];
+		right_walk = new TextureRegion[temporary.length];
+		
+		int left_count = temporary.length - 1;
+		int counter = 0;
+		int right_count = 0;
+		
+		while(left_count >= 0)
+			left_walk[counter++] = temporary[0][left_count--];
+			
+		counter = 0;
+		while(right_count < temporary.length)
+			right_walk[counter++] = temporary[0][right_count++];
+		
+		//Initialize sprite
 		final float width = 0.75f;
 		final float height = 1.5f;
 		
-		final float head_width = width;
-		final float head_height = head_width;
-		final float head_start_y = screen.player_start_point.y + height - head_height;		
-		head = new Head(screen.game.assets.getTexture(Assets.TEXTURE_FACE));
-		head.setBounds(screen.player_start_point.x, head_start_y, head_width, head_height);
+		sprite = new Sprite(right_walk[0]);
+		sprite.setBounds(screen.player_start_point.x, screen.player_start_point.y, width, height);
 		
-		final float body_width = width;
-		final float body_height = height - head_width;
-		body = new Body(screen.game.assets.getTexture(Assets.TEXTURE_CIRCLE));
-		body.setBounds(screen.player_start_point.x, screen.player_start_point.y, body_width, body_height);
-		
-		final float hand_size = body_height * 0.25f;
-		hand = new Hand(screen.game.assets.getTexture(Assets.TEXTURE_CIRCLE));
-		hand.setBounds(screen.player_start_point.x + width / 2f - hand_size / 2f, screen.player_start_point.y + height / 2f - hand_size, hand_size, hand_size);
 	}
 
 	@Override
@@ -45,19 +61,47 @@ public abstract class Humanoid extends Player
 	{
 		if(moving_left)
 		{
-			head.translateX(-1f * delta);
+			//Physical translation
+			is_facing_left = true;
+			sprite.translateX(-1f * delta);
+			
+			//Spritesheet translation
+			animation_timer += delta;
+			if(animation_timer >= animation_tick / left_walk.length)
+			{
+				animation_timer -= animation_tick / left_walk.length;
+				sprite.setTexture(left_walk[current_animation_counter++].getTexture());
+			}
 		}
-		if(moving_right)
+		else if(moving_right)
 		{
-			head.translateX(1f * delta);
+			//Physical translation
+			is_facing_left = true;
+			sprite.translateX(1f * delta);
+			
+			//Spritesheet translation
+			animation_timer += delta;
+			if(animation_timer >= animation_tick / left_walk.length)
+			{
+				animation_timer -= animation_tick / left_walk.length;
+				sprite.setTexture(left_walk[current_animation_counter++].getTexture());
+			}
+		}
+		else
+		{
+			animation_timer = 0f;
+			current_animation_counter = 0;
+				
+			if(is_facing_left)
+				sprite.setTexture(left_walk[0].getTexture());
+			else
+				sprite.setTexture(right_walk[0].getTexture());
 		}
 	}
 
 	@Override
 	public void draw(SpriteBatch batch)
 	{
-		body.draw(batch);
-		hand.draw(batch);
-		head.draw(batch);
+		sprite.draw(batch);
 	}
 }
