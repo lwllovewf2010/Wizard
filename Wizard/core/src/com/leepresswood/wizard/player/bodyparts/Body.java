d48011ec04b003b3695b8e00bdf4994433073b72
Lee Presswood <pressedlee123@gmail.com>
2015-01-22 19:27:03 -0600

    More rearranging.

--- Wizard/core/src/com/leepresswood/wizard/player/bodyparts/Body.java
+++
@@ -1,9 +0,0 @@
-package com.leepresswood.wizard.player.bodyparts;
-
-public class Body
-{
-	public Body()
-	{
-		
-	}	
-}
--- Wizard/core/src/com/leepresswood/wizard/player/bodyparts/Hand.java
+++
@@ -1,9 +0,0 @@
-package com.leepresswood.wizard.player.bodyparts;
-
-public class Hand
-{
-	public Hand()
-	{
-		
-	}
-}
