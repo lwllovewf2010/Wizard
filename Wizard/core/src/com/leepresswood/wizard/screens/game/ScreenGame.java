27fcb5bdaa60576f5f6a916e01ac146e94535f76
Lee Presswood <pressedlee123@gmail.com>
2015-01-26 02:05:23 -0600

    Created cameras for the aspects of the game.

--- Wizard/core/src/com/leepresswood/wizard/screens/game/ScreenGame.java
+++ Wizard/core/src/com/leepresswood/wizard/screens/game/ScreenGame.java
@@ -1,86 +1,38 @@
 package com.leepresswood.wizard.screens.game;
 
-import java.util.ArrayList;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Vector2;
 import com.leepresswood.wizard.GameWizard;
 import com.leepresswood.wizard.data.Assets;
 import com.leepresswood.wizard.entities.player.Player;
-import com.leepresswood.wizard.input.Input;
 import com.leepresswood.wizard.screens.ScreenParent;
 
 public class ScreenGame extends ScreenParent
 {
 	public GUIGame gui;
-	public ShapeRenderer renderer;
 
 	public TiledMap map;
 	public OrthogonalTiledMapRenderer map_renderer;
 	
-	public OrthographicCamera camera;
-	public final int WORLD_VIEW = 25;
-	public final int WORLD_TOTAL_HORIZONTAL;
-	public final int WORLD_TOTAL_VERTICAL;
+	public int WORLD_VIEW = 25;
+	public int WORLD_TOTAL_HORIZONTAL;
+	public int WORLD_TOTAL_VERTICAL;
 	
 	public Vector2 player_start_point;
 	public Player player;
-	
-	public ArrayList<Object> remove;
 	
 	public ScreenGame(GameWizard game)
 	{
 		super(game);
 		
-		Gdx.input.setInputProcessor(new Input(this));
-		renderer = new ShapeRenderer();
-		
 		gui = new GUIGame(this);
-		
-		//Map stuff. See here: https://github.com/libgdx/libgdx/wiki/Tile-maps
-		map = game.assets.getMap(Assets.MAP_TEST);							//Load map
-		map_renderer = new OrthogonalTiledMapRenderer(map, 1f / new Float(map.getProperties().get("tilewidth", Integer.class)));	//Draws passed map. Passed float number is the the inverse of the pixels per unit.
-		
-		WORLD_TOTAL_HORIZONTAL = map.getProperties().get("width", Integer.class);
-		WORLD_TOTAL_VERTICAL = map.getProperties().get("height", Integer.class);
-		
-		//Create the camera using the found number of blocks above.
-		camera = new OrthographicCamera(WORLD_VIEW, WORLD_VIEW * Gdx.graphics.getHeight() / Gdx.graphics.getWidth());
-		camera.position.set(camera.viewportWidth / 2f, camera.viewportHeight / 2f, 0);
-		camera.update();
 		
 		player_start_point = new Vector2(0f, 2f);
 		//player = new AirWizard(this);
-		
-		remove = new ArrayList<Object>();
-	}
-	
-	@Override
-	public void render(float delta)
-	{
-		//Update
-		update(delta);
-		
-		//Draw
-		Gdx.gl.glClearColor(1, 1, 1, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);		
-		
-		//Map
-		camera.update();
-		map_renderer.setView(camera);
-		map_renderer.render();
-		
-		//Sprites
-		renderer.setProjectionMatrix(camera.combined);
-		player.draw(renderer);
-		
-		//GUI
-		gui.draw();
 	}
 	
 	@Override
@@ -99,6 +51,22 @@ public class ScreenGame extends ScreenParent
 	@Override
 	public void setUpCameras()
 	{
+		//Map stuff. See here: https://github.com/libgdx/libgdx/wiki/Tile-maps
+		map = game.assets.getMap(Assets.MAP_TEST);							//Load map
+		map_renderer = new OrthogonalTiledMapRenderer(map, 1f / new Float(map.getProperties().get("tilewidth", Integer.class)));	//Draws passed map. Passed float number is the the inverse of the pixels per unit.
+		
+		//Create the camera using the found number of blocks above.
+		WORLD_TOTAL_HORIZONTAL = map.getProperties().get("width", Integer.class);
+		WORLD_TOTAL_VERTICAL = map.getProperties().get("height", Integer.class);		
+		camera_game = new OrthographicCamera(WORLD_VIEW, WORLD_VIEW * Gdx.graphics.getHeight() / Gdx.graphics.getWidth());
+		camera_game.position.set(camera_game.viewportWidth / 2f, camera_game.viewportHeight / 2f, 0);
+		camera_game.update();
+		
+		//Create a camera for the GUI.
+		camera_gui = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		camera_gui.position.x = camera_gui.viewportWidth / 2f;
+		camera_gui.position.y = camera_gui.viewportHeight / 2f;
+		camera_gui.update();
 	}
 	
 	@Override
@@ -114,6 +82,15 @@ public class ScreenGame extends ScreenParent
 
 	@Override
 	public void draw()
-	{
+	{		
+		//Map
+		map_renderer.setView(camera_game);
+		map_renderer.render();
+		
+		//Game objects
+		renderer.setProjectionMatrix(camera_game.combined);		
+		
+		//GUI
+		gui.draw();
 	}
 }
