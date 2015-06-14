2c9026b11f331467975700d90abd4bd02150fb0e
Lee Presswood <pressedlee123@gmail.com>
2015-06-14 14:48:49 -0500

    Added button level class.

--- Wizard/core/src/com/leepresswood/wizard/screens/game/gui/GUIButtonLevelClass.java
+++ Wizard/core/src/com/leepresswood/wizard/screens/game/gui/GUIButtonLevelClass.java
@@ -3,9 +3,13 @@ package com.leepresswood.wizard.screens.game.gui;
 import com.badlogic.gdx.graphics.Texture;
 import com.leepresswood.wizard.screens.game.ScreenGame;
 
+/**
+ * The button that opens the level shop. 
+ * @author Lee
+ *
+ */
 public class GUIButtonLevelClass extends GUIButton
 {
-	
 	public GUIButtonLevelClass(ScreenGame screen, Texture t, float x, float y, float width, float height)
 	{
 		super(screen, t, x, y, width, height);
@@ -21,6 +25,7 @@ public class GUIButtonLevelClass extends GUIButton
 	@Override
 	public void doClick()
 	{
+		//Replace the current screen with the shop screen.
 		
 	}
 }
