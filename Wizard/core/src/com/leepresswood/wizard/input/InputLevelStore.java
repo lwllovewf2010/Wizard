884c80b262c171e9443e7b96f53061ab155545d9
Lee Presswood <pressedlee123@gmail.com>
2015-06-23 08:02:27 -0500

    Input on level store screen now works.

--- Wizard/core/src/com/leepresswood/wizard/input/InputLevelStore.java
+++ Wizard/core/src/com/leepresswood/wizard/input/InputLevelStore.java
@@ -1,5 +1,6 @@
 package com.leepresswood.wizard.input;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.leepresswood.wizard.screens.game.gui.GUIButton;
 import com.leepresswood.wizard.screens.levelstore.ScreenLevelStore;
@@ -34,9 +35,16 @@ public class InputLevelStore implements InputProcessor
 	@Override
 	public boolean touchDown(int screenX, int screenY, int pointer, int button)
 	{
+		//Corrent screenY before doing the touch.
+		screenY = Gdx.graphics.getHeight() - screenY;
+		
 		for(GUIButton b : screen.button_array)
+		{	
 			if(b.is_active && b.sprite.getBoundingRectangle().contains(screenX, screenY))
+			{
 				b.doClick();
+			}
+		}
 		
 		return true;
 	}
--- Wizard/core/src/com/leepresswood/wizard/screens/levelstore/ScreenLevelStore.java
+++ Wizard/core/src/com/leepresswood/wizard/screens/levelstore/ScreenLevelStore.java
@@ -1,7 +1,6 @@
 package com.leepresswood.wizard.screens.levelstore;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
