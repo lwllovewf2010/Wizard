509f5a367a6cc257c15445f0a3f99ea5eb366f2c
Lee Presswood <LeePresswood@gmail.com>
2015-07-30 10:58:35 -0500

    Added big explanation of my goal for leveling up.

--- Wizard/core/src/com/leepresswood/wizard/handlers/LevelHandler.java
+++ Wizard/core/src/com/leepresswood/wizard/handlers/LevelHandler.java
@@ -14,6 +14,41 @@ public class LevelHandler
 	public int level;										//Total points available.
 	public int points_spent;							//Points that have been spent on leveling.
 	
+	/**
+	 * Level up logic:
+	 * There will be three paths of level ups for each wizard. Each path will have
+	 * around six skills. Each skill requires that path's previous skill in order
+	 * to be chosen. The skills are broken into six "levels" where level one is
+	 * the base level for that skill. The number of levels into that path will
+	 * directly relate to the cost of that skill. That is to say that the cost
+	 * goes up as you move through the levels of that path. This is for balance.
+	 * 
+	 * The paths will represent three different versions of fighting strategy.
+	 * They will probably be named something clever as the game-making process
+	 * goes on, but the general idea is that there will be a skillset based upon
+	 * direct damage (fireball, aether orb, arrows, fire wall), another based upon 
+	 * indirect damage (traps, mines, tricks, summons), and one based upon defense
+	 * (lower damage taken, damage nearby enemies with a burn)
+	 * 
+	 * There will be a (few?) subpath(s) for each of the three main paths. They
+	 * will upgrade the path's effects without raising the base path skill.
+	 * As an example, the Void Wizard's main attack spell is a floating ball.
+	 * Leveling up the main path will increase the number of balls that are released
+	 * from the Wizard. Leveling the subpath will enhance the properties of each
+	 * orb, such as increasing the speed, making the orbs heat-seeking, or
+	 * adding more knockback.
+	 * 
+	 * Not every every skill obtained will increase the number of available castable
+	 * spells on the game screen, but some of them will.
+	 * 
+	 * As far as implementation is concerned, new spells should be added to an
+	 * ArrayList. New effects should be added as a Buff.
+	 */
+	
+	
+	
+	
+	//Old 
 	public final int SPELLS_NUMBER_MAX = 5;		//Maximum spells the player can have.
 	public int spells_available;						//Number of available spells.
 	
