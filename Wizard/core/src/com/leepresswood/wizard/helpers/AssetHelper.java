2ba12f16267cddbbaf2e51df5b6197a7786c7149
Lee Presswood <pressedlee123@gmail.com>
2015-08-04 23:07:13 -0500

    Extracted more strings.

--- Wizard/core/src/com/leepresswood/wizard/helpers/AssetHelper.java
+++ Wizard/core/src/com/leepresswood/wizard/helpers/AssetHelper.java
@@ -14,8 +14,22 @@ public class AssetHelper extends AssetManager
 {
 	public static final int MAP_TEST = 0;
 	
+	//Extracted strings.
+	//Spells.
 	public static final String TEXTURE_SPELL_AETHER = "textures/spells/aether.png";
-	public static final String TEXTURE_SPELL_FIREBALL = "textures/spells/aether.png";
+	public static final String TEXTURE_SPELL_FIREBALL = "textures/spells/fireball.png";
+	public static final String TEXTURE_SPELL_HOLD = "textures/spells/hold.png";
+	
+	//Enemies.
+	
+	
+	//Parallax.
+	public static final String TEXTURE_PARALLAX_BACK1 = "textures/parallax/smallforest/back1.png";
+	public static final String TEXTURE_PARALLAX_BACK2 = "textures/parallax/smallforest/back2.png";
+	public static final String TEXTURE_PARALLAX_BACK3 = "textures/parallax/smallforest/back3.png";
+	public static final String TEXTURE_PARALLAX_MIDDLE1 = "textures/parallax/smallforest/middle1.png";
+	public static final String TEXTURE_PARALLAX_MIDDLE2 = "textures/parallax/smallforest/middle2.png";
+	public static final String TEXTURE_PARALLAX_SKY = "textures/parallax/smallforest/sky.png";
 	
 	public AssetHelper()
 	{
@@ -37,16 +51,16 @@ public class AssetHelper extends AssetManager
 		
 		//Spell.
 		load(TEXTURE_SPELL_AETHER, Texture.class);
-		load("textures/spells/fireball.png", Texture.class);
-		load("textures/spells/hold.png", Texture.class);
+		load(TEXTURE_SPELL_FIREBALL, Texture.class);
+		load(TEXTURE_SPELL_HOLD, Texture.class);
 		
 		//Parallax. http://imageresize.org/Default.aspx
-		load("textures/parallax/smallforest/back1.png", Texture.class);
-		load("textures/parallax/smallforest/back2.png", Texture.class);
-		load("textures/parallax/smallforest/back3.png", Texture.class);
-		load("textures/parallax/smallforest/middle1.png", Texture.class);
-		load("textures/parallax/smallforest/middle2.png", Texture.class);
-		load("textures/parallax/smallforest/sky.png", Texture.class);
+		load(TEXTURE_PARALLAX_BACK1, Texture.class);
+		load(TEXTURE_PARALLAX_BACK2, Texture.class);
+		load(TEXTURE_PARALLAX_BACK3, Texture.class);
+		load(TEXTURE_PARALLAX_MIDDLE1, Texture.class);
+		load(TEXTURE_PARALLAX_MIDDLE2, Texture.class);
+		load(TEXTURE_PARALLAX_SKY, Texture.class);
 	}
 	
 	public TiledMap getMap(int map)
